4b6d8f69f9d62b724f5714121b74ee4cb29bb684
bjawaberh@gmail.com <bjawaberh@gmail.com>
2022-02-03 17:01:41 +0200

    loops pro

---
+++ main.js
@@ -0,0 +1,29 @@
+//let count = 1000
+//let i = 0
+
+//while (count >= 1) {
+
+   // console.log(count);
+     //count = count +1
+    ///count++
+    //count += 2
+    //count *=2
+   // count /= 2
+   // i++
+ 
+
+//}
+
+//let msg ='finished executing the loop in '
+//msg+= i + ' iterations'
+//console.log(msg);
+
+const limit =10
+let stars ="*"
+let count = 1
+while (count <=limit){
+console.log(stars);
+stars+= "*"
+count++
+
+}
